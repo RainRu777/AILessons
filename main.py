4b31cbdeffcff131ca9c3df7b0b7b7aeb425590a
Mikhail Bortsov <58608524+RainRu777@users.noreply.github.com>
2023-06-05 11:37:44 +0300

    'Good day' added

--- main.py
+++ main.py
@@ -13,5 +13,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('Mike')
+    print('Ist it a good day?')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
